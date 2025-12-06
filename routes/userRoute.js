39f9dab785a05099a16223fa05c3047b2c6e9c3b
Ajen M <diajengwulan2000@gmail.com>
2025-12-06 16:56:12 +0700

    first commit

---
+++ routes/userRoute.js
@@ -0,0 +1,12 @@
+const express = require('express') // untuk memuat router
+const userControl = require('../controllers/userController')
+
+const router = express.Router()
+
+router.get('/',userControl.getAllBooks)
+router.get('/:code',userControl.getBookByCode)
+
+router.post('/', userControl.addBook)
+router.delete('/:code', userControl.delBook)
+
+module.exports = router
